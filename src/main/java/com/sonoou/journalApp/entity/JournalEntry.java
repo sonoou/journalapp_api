993445a156d35e9e0c75a02b205058b65bcb5f85
sonoou <sonu26681094@gmail.com>
2025-11-30 11:23:51 +0530

    Added dependency for lombok

--- src/main/java/com/sonoou/journalApp/entity/JournalEntry.java
+++ src/main/java/com/sonoou/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.sonoou.journalApp.entity;
 
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,44 +9,16 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document
+@Data
 public class JournalEntry {
 
     @Id //not mandatory
     private ObjectId id;
+
     private String title;
+
     private String content;
 
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
